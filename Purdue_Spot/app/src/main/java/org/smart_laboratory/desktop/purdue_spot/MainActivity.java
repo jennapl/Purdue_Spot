f6cd6b3c67a3e3bd4fd48cce14683cb7d6ec8ffe
jennapl <jplusk21@gmail.com>
2023-11-04 16:29:12 -0400

    Empty Project Push
    Pushing empty project for development.

---
+++ Purdue_Spot/app/src/main/java/org/smart_laboratory/desktop/purdue_spot/MainActivity.java
@@ -0,0 +1,14 @@
+package org.smart_laboratory.desktop.purdue_spot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
